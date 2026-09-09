41c690947cf01191a521d68e9addeaa789ba07a6
venixbr <viniciusschultz@localhost.localdomain>
2026-09-09 15:19:40 -0300

    adding sdf_cmd_file

---
+++ synthesis/outputs/deliverables/last/sdf_cmd_file.cmd
@@ -0,0 +1,10 @@
+// SDF command file frontend/sdf_cmd_file.cmd
+
+COMPILED_SDF_FILE = "../outputs/deliverables/atrasos.sdf.X",
+SCOPE = "circuito_tb.DUT",
+LOG_FILE = "sdf.log",
+MTM_CONTROL = "MAXIMUM",
+SCALE_FACTORS = "1.0:1.0:1.0",
+SCALE_TYPE = "FROM_MTM";
+
+// END OF FILE: frontend/sdf_cmd_file.cmd
